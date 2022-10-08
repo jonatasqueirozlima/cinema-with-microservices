810af449dbee9afc34066f7b0c98cd11afd396cb
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-10-08 16:22:35 -0300

    feat: :sparkles: initialize the server

---
+++ catalog-service/src/index.js
@@ -0,0 +1,14 @@
+
+(async () => {
+	require("dotenv-safe").config();
+	const cinemaCatalog = require('./api/catalog');
+	const server = require("./server/server");
+	const repository = require("./repository/repository");
+
+	try {
+		await server.start(cinemaCatalog, repository);
+		console.log('Server is up and running at ' + process.env.PORT);
+	} catch (error) {
+		console.error(error);
+	}
+})();
