23c90a471f62ae4368c38761ccda6e59dfaa3716
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-10-08 14:52:31 -0300

    test: :white_check_mark: database connection and disconnection

---
+++ catalog-service/src/config/database.test.js
@@ -0,0 +1,12 @@
+require('dotenv-safe').config();
+const database = require('./database');
+
+test('MongoDB Connection', async () => {
+	const connection = await database.connect();
+	expect(connection).toBeTruthy();
+})
+
+test('MongoDB Disconnection', async () => {
+	const isDisconnected = await database.disconnect();
+	expect(isDisconnected).toBeTruthy();
+})
